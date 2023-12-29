9394e2e8974aa59a80ce6074ca24d4dc4c540ffe
Abrar Alam <abraralam246@gmail.com>
2023-12-28 20:37:09 -0500

    add local storage features

--- src/script.js
+++ src/script.js
@@ -81,9 +81,20 @@ function createDivsForColors(colorArray) {
     i++;
   }
 
-  totalNumberOfCards = colorGridDiv.childElementCount;
+  
 }
 
+// This function takes care of loading the game screen for the first time with 
+// best score value so far. 
+// Params: cards: an array of shuffled cards
+function setupGame(cards){
+  createDivsForColors(cards);
+  totalNumberOfCards = colorGridDiv.childElementCount;
+  // Load the best score value from local storage
+  if (localStorage.getItem("bestScore")){
+    gameStartScreen.querySelector("h3#best-score-val").innerText = JSON.parse(localStorage.getItem("bestScore"));
+  }
+}
 
 gameStartScreen.querySelector("button#start-game-button").addEventListener("click", handleStartGame);
 
@@ -93,14 +104,14 @@ function handleStartGame(event){
   gameStartScreen.style.display = "none";
   liveScoreDiv.style.display = "block";
   liveScoreDiv.querySelector("h3#score-value").innerText = score;
+  gameOverScreen.style.display = "none" ;
 }
 
 
 
 
 function handleCardClick(event) {
-    // let timeoutID = null;
-    // let cardsRevealed = 0;
+    //Keep updating the live score
     liveScoreDiv.querySelector("h3#score-value").innerText = ++score;
 
     if (realTimeData.guessCounter === 0){
@@ -126,30 +137,38 @@ function handleCardClick(event) {
       realTimeData.guessCounter++;
       realTimeData.indices[1] = event.target.dataset.idx;
       realTimeData.types[1] = event.target.getAttribute("class");
+      // check if two cards are match.
+      if(realTimeData.types[0] === realTimeData.types[1]){
+        console.log("Timeout ID ", realTimeData.timeoutID);
+        clearTimeout(realTimeData.timeoutID);
+        // detach the event listeners
+        event.target.removeEventListener("click", handleCardClick);
+        colorGridDiv.querySelector(`div[data-idx = '${realTimeData.indices[0]}']`).removeEventListener("click",handleCardClick);
+        // Resetting back the counter to 0 since we have a match
+        realTimeData.guessCounter = 0;
+        cardsRevealed += 2;
 
-      if (realTimeData.guessCounter === 2){
-        // check if two cards are match.
-        if(realTimeData.types[0] === realTimeData.types[1]){
-          console.log("Timeout ID ", realTimeData.timeoutID);
-          clearTimeout(realTimeData.timeoutID);
-          // detach the event listeners
-          event.target.removeEventListener("click", handleCardClick);
-          colorGridDiv.querySelector(`div[data-idx = '${realTimeData.indices[0]}']`).removeEventListener("click",handleCardClick);
-          // Resetting back the counter to 0 since we have a match
-          realTimeData.guessCounter = 0;
-          cardsRevealed += 2;
-          gameOverScreen.style.display = (cardsRevealed === totalNumberOfCards) ? "block" : "none";
-
+        // Check if the player has finished playing. If so update some display
+        // properties, and save the score 
+        if (cardsRevealed === totalNumberOfCards){
+          gameOverScreen.style.display = "block" ;
+          // if a previous score does not exist or the current score is lower than a previous non
+          // zero score, then update the local storage
+          if ((!localStorage.getItem("bestScore")) || ((score !== 0) && (JSON.parse(localStorage.getItem("bestScore")) > score))){
+            localStorage.setItem("bestScore", score);
+          }
         }
-        else {
-          clearTimeout(realTimeData.timeoutID);
-          setTimeout(function(){
-            event.target.style.backgroundColor = "white";
-            colorGridDiv.querySelector(`div[data-idx = '${realTimeData.indices[0]}']`).style.backgroundColor = "white";
-            realTimeData.guessCounter = 0;
-          }, 1500);
-        }
+        
       }
+      else {
+        clearTimeout(realTimeData.timeoutID);
+        setTimeout(function(){
+          event.target.style.backgroundColor = "white";
+          colorGridDiv.querySelector(`div[data-idx = '${realTimeData.indices[0]}']`).style.backgroundColor = "white";
+          realTimeData.guessCounter = 0;
+        }, 1500);
+      }
+      
 
     }
 
@@ -200,4 +219,4 @@ function handleCardClick(event) {
 
 
 // when the DOM loads
-createDivsForColors(shuffledColors);
+setupGame(shuffledColors);
